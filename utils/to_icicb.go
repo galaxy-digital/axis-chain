1a56a54ce9177db33d021f9e21699a7061dd7781
galaxy126 <galaxy85126@gmail.com>
2022-04-02 17:25:02 +0100

    initial commit

--- utils/to_icicb.go
+++
@@ -1,8 +0,0 @@
-package utils
-
-import "math/big"
-
-// ToAxis number of AXIS to Wei
-func ToAxis(axis uint64) *big.Int {
-	return new(big.Int).Mul(new(big.Int).SetUint64(axis), big.NewInt(1e18))
-}
